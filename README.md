a19f4a8125ac5e0da374e0a74686856c382aef36
LuisMiguelBlancoP <ing.luis.blanco18@gmail.com>
2024-01-21 18:39:52 -0500

    commit inicial dominio de alerta

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# alert_backend
-Alert domain backend
+## Alert Java Lambdas
+
+## Description
+
+## Requirements
+
+- [AWS CLI](https://aws.amazon.com/cli/)
+- [AWS SAM](https://aws.amazon.com/serverless/sam/)
+- Java:
+    - v17
+- Maven
+
+## Deployment
+
+Deploy the application to your AWS account using [AWS SAM](https://aws.amazon.com/serverless/sam/).
+
+```bash
+cmd.exe /c deployment\scripts\deploy.bat
+```
---
+++ deployment/scripts/deploy.bat
@@ -0,0 +1,35 @@
+@echo off
+
+cd %~dp0
+cd ../..
+
+echo Initializing params...
+
+SET STACK_NAME=alert-stack
+SET PARAMS=--resolve-s3 --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
+SET TEMPLATE_PATH=deployment\template\template.yaml
+
+echo The stack name to be created/updated is %STACK_NAME%...
+
+echo Creating JAR file to upload...
+call mvnw package
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo The artifact creation has failed.
+    exit /b %ERRORLEVEL%
+)
+echo JAR File Created...
+
+echo Ruta actual: %CD%
+
+SET FINAL_PARAMS=--template-file %TEMPLATE_PATH% --stack-name %STACK_NAME% %PARAMS%
+
+echo The creation/update of the Stack %STACK_NAME% will start with the next params [%FINAL_PARAMS%]
+sam deploy %FINAL_PARAMS%
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo The stack deployment has failed.
+    exit /b %ERRORLEVEL%
+)
+
+echo The creation/update of the Stack %STACK_NAME% has finished successfully
